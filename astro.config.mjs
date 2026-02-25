78e5f3390e9ea9caded166c35261234af723d167
Andrii Gudz <andrey.gudz@gmail.com>
2026-02-25 20:31:55 +0100

    basic seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cutly-app.com/',
-  integrations: [icon()],
+  site: 'https://cutly-app.com',
+  integrations: [
+    icon(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en',
+          de: 'de',
+          uk: 'uk',
+          ru: 'ru',
+        },
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()]
   }
